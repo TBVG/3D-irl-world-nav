09ae5f9ee77c215c5ef295ce1ddaf0824b646782
TBVG <tariq.biviji@gmail.com>
2024-01-03 02:51:10 -0500

    access token modified

--- mapbox_impl.py
+++ mapbox_impl.py
@@ -1,7 +1,7 @@
 import mapbox
 from mapbox import Geocoder, Directions
 import mapboxgl
-
+access_token = 'pk.eyJ1IjoidGFyaXFiaXZpamkiLCJhIjoiY2xsNTY0d3JtMGQxdjNlbzF2b2loNm40aCJ9.AZajFCsj0ImNT2zgkcF45Q'
 # Set access token
 def set_api_keys(access_token):
   mapbox.access_token = access_token
